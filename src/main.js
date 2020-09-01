1f329de29373e5a098e834b9bcac350ddcebf599
Star Coder <starcoder204@gmail.com>
2020-09-01 20:26:39 +0200

    test commit

--- src/main.js
+++ src/main.js
@@ -7,12 +7,23 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import VueI18n from 'vue-i18n'
+import messages from './lang'
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
+Vue.use(VueI18n)
+export const i18n = new VueI18n({
+  locale: 'de',
+  fallbackLocale: 'de',
+  messages
+})
+
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+import { i18n } from '../main.js'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/:lang',
+    component: {
+      template: '<router-view></router-view>'
+    },
+    beforeEnter (to, from, next) {
+      const lang = to.params.lang
+      if (!['en', 'de'].includes(lang)) return next('de')
+      if (i18n.locale !== lang) {
+        i18n.locale = lang
+      }
+      return next()
+    },
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: About
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    redirect: '/de'
   }
 ]
 
